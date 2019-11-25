2479701597ad5c6d5c5c1f0f899c9045a59413a6
c-v_polyatykyn <Vladyslav.Polyatykyn@mindgeekconsultants.com>
2019-11-25 17:08:55 +0200

    Added ability to add items to redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addItem } from '../../redux/cart/cart.actions';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl}) => (
-    <div className='collection-item'>
-        <div 
-            className="image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className="collection-footer">
-        <span className='name'>{name}</span>
-        <span className='price'>${price}</span>
+import CustomButton from '../custom-button/custom-button.component';
+
+const CollectionItem = ({ item }) => {
+    const dispatch = useDispatch();
+    const { name, price, imageUrl } = item;
+
+    return (
+        <div className='collection-item'>
+            <div 
+                className="image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className="collection-footer">
+            <span className='name'>{name}</span>
+            <span className='price'>${price}</span>
+            </div>
+            <CustomButton inverted onClick={() => dispatch(addItem(item))}>
+                Add to cart
+            </CustomButton>
         </div>
-    </div>
-);
+    );
+};
 
 export default CollectionItem;
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`custom-button ${isGoogleSignIn ? 'google-sign-in' : ''}`} {...otherProps} >
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <button 
+        className={
+            `custom-button 
+            ${isGoogleSignIn ? 'google-sign-in' : ''}
+            ${inverted ? 'inverted' : ''}`
+        }
+        {...otherProps} >
         {children}
     </button>
 );
